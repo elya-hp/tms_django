4ee1bb184c51f2e091ed308e8b7236bba423b9b3
elya-hp <elya.hp@gmail.com>
2024-04-15 20:25:03 +0100

    adding user model

--- tms_django/users/models.py
+++ tms_django/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import TextChoices
 
-# Create your models here.
+
+class User(AbstractUser):
+    class UserType(TextChoices):
+        DRIVER = "driver"
+        DISPATCHER = "dispatcher"
+
+    user_type = models.CharField(max_length=100, choices=UserType, null=True)

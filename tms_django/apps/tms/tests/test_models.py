1e4e7a10771835f6096597c2221720f214a8266f
elya-hp <elya.hp@gmail.com>
2024-04-16 16:27:05 +0100

    fixing tests

--- tms_django/apps/tms/tests/test_models.py
+++ tms_django/apps/tms/tests/test_models.py
@@ -3,21 +3,14 @@ from django.test import TestCase
 
 
 class TestTruckModel(TestCase):
-    def setUp(self):
-        self.truck = Truck.objects.create(kind=TruckKind.SMALL_STRAIGHT, weight=5000, capacity=2000)
-
     def test_truck_create(self):
-        self.assertEqual(self.truck.kind, TruckKind.SMALL_STRAIGHT)
-        self.assertEqual(self.truck.weight, 5000)
-        self.assertEqual(self.truck.capacity, 2000)
-
-    def test_truck_str(self):
-        self.assertEqual(str(self.truck), "small straight")
+        truck = Truck.objects.create(kind=TruckKind.SMALL_STRAIGHT, weight=5000, capacity=2000)
+        self.assertEqual(truck.kind, TruckKind.SMALL_STRAIGHT)
+        self.assertEqual(truck.weight, 5000)
+        self.assertEqual(truck.capacity, 2000)
 
 
 class TestBrokerModel(TestCase):
-    def setUp(self):
-        self.broker = Broker.objects.create(agent_name="Angela K", company_name="Midex Corp")
-
     def test_broker_create(self):
-        self.assertEqual(self.broker, Broker.objects.get())
+        broker = Broker.objects.create(agent_name="Angela K", company_name="Midex Corp")
+        self.assertIsNotNone(broker.id)

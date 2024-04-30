be20b2c48a1541c9929af75bfb177bb46db87def
elya-hp <elya.hp@gmail.com>
2024-04-30 20:39:33 +0100

    adding tests for id generators

--- tms_django/apps/tms/tests/test_models.py
+++ tms_django/apps/tms/tests/test_models.py
@@ -1,3 +1,4 @@
+from apps.tms.generators import generate_model_custom_id
 from apps.tms.models import BookedLoad, Broker, DispatcherProfile, DriverProfile, Truck
 from apps.tms.tests.factories import (
     BookedLoadFactory,
@@ -8,6 +9,35 @@ from apps.tms.tests.factories import (
 )
 from apps.users.models import User
 from django.test import TestCase
+from loguru import logger
+
+
+class TestIdGenerator(TestCase):
+    def test_generate_model_custom_id__no_records(self):
+        self.assertFalse(DriverProfile.objects.all().exists(), "clean your DriverProfile db records")
+        default_value = 101
+        expected = str(default_value)
+
+        result = generate_model_custom_id(
+            model_name="DriverProfile",
+            custom_id_field_name="unit_id",
+            default_value=default_value,
+        )
+        self.assertEqual(result, expected)
+
+    def test_generate_model_custom_id__with_records(self):
+        dp1 = DriverProfileFactory()
+        dp2 = DriverProfileFactory()
+        logger.info(f"{dp1}, {dp2}")
+        expected = str(int(dp2.unit_id) + 1)
+
+        result = generate_model_custom_id(
+            model_name="DriverProfile",
+            custom_id_field_name="unit_id",
+            default_value=1000,
+        )
+
+        self.assertEqual(result, expected)
 
 
 class TestTruckModel(TestCase):

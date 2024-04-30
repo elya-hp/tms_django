b03768f2879ecb1e265245649a7e94e081c14252
elya-hp <elya.hp@gmail.com>
2024-04-30 18:08:01 +0100

    adding Faker

--- tms_django/apps/users/tests/factories.py
+++ tms_django/apps/users/tests/factories.py
@@ -12,6 +12,8 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     username = factory.Faker("user_name")
     email = factory.Faker("email")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
 
 
 class DispatcherUserFactory(UserFactory):

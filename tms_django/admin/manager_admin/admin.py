c7ae4f2cf98fb571ab11aed412b770fbc7ca92da
elya-hp <elya.hp@gmail.com>
2024-05-01 19:33:40 +0100

    adding Broker and Truck to Admin panel

--- tms_django/admin/manager_admin/admin.py
+++ tms_django/admin/manager_admin/admin.py
@@ -1,3 +1,12 @@
+from apps.tms.models import (
+    Billing,
+    BookedLoad,
+    Broker,
+    DispatcherProfile,
+    DriverProfile,
+    Truck,
+)
+from apps.users.models import User
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 
@@ -11,3 +20,21 @@ class ManagerAdminSite(AdminSite):
 manager_admin = "manager_admin"
 
 manager_admin_site = ManagerAdminSite(name=manager_admin)
+
+
+@admin.register(Broker)
+class BrokerAdmin(admin.ModelAdmin):
+    list_display = (
+        "company_name",
+        "agent_name",
+        "mc_number",
+    )
+
+
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    list_display = (
+        "kind",
+        "weight",
+        "capacity",
+    )

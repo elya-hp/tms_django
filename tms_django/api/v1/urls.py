8ed6158f6f907b85105faad79260db48bc7ce727
elya-hp <elya.hp@gmail.com>
2024-05-23 14:10:09 +0100

    fixing api urls

--- tms_django/api/v1/urls.py
+++ tms_django/api/v1/urls.py
@@ -1,6 +1,15 @@
+from api.v1 import views
 from django.urls import include, path
 from rest_framework import routers
 
 app_name = "v1"
+router = routers.DefaultRouter()
 
-urlpatterns = []
+router.register(r"driver", views.DriverProfileViewSet)
+router.register(r"dispatcher", views.DispatcherProfileViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("count_miles/", views.MilesCountViewSet.as_view(), name="count_miles"),
+]
